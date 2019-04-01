033de8cdd57ab98241f5e83abdd7ee00ed9a9791
Daniel <danielstarling4@gmail.com>
2019-04-01 15:53:25 -0700

    fixed login

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Register from './Register';
-import Login from './Register';
+import Login from './Login';
 
 class App extends Component {
   render() {
--- app/src/Login.js
+++ app/src/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class Login extends Component {
   constructor() {
     super();
@@ -8,13 +8,49 @@ class Login extends Component {
       password: ''
     };
   }
+  inputChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  loginUser = () => {
+    axios
+      .post('http://localhost:5000/api/login', {
+        username: this.state.username,
+        password: this.state.password
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
       <div>
         <form>
-          <input type="text" placeholder="username" />
-          <input type="password" placeholder="password" />
-          <button type="submit">Submit</button>
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={this.username}
+            onChange={e => this.inputChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={this.password}
+            onChange={e => this.inputChange(e)}
+            name="password"
+          />
+          <button
+            onClick={e => {
+              e.preventDefault();
+              this.loginUser();
+            }}
+          >
+            Submit
+          </button>
         </form>
       </div>
     );
