fd995b188f0c8a1d9832af641c92af270e57ccd3
Daniel <danielstarling4@gmail.com>
2019-04-02 12:36:43 -0700

    sessions built cookies working, refactored project into smaller files

--- index.js
+++ index.js
@@ -1,94 +1,77 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const bcrypt = require('bcryptjs'); // installed this
-const server = express();
-const db = require('./data/dbConfig.js');
-const Users = require('./users/userHelpers.js');
+// const express = require('express');
+// const helmet = require('helmet');
+// const cors = require('cors');
+// const bcrypt = require('bcryptjs'); // installed this
+// const Users = require('./userHelpers.js');
+const server = require('./api/server');
 
-server.use(helmet());
-server.use(express.json());
-server.use(cors());
-server.get('/', (req, res) => {
-  res.send('API ONLINE');
-});
+// server.use(helmet());
+// server.use(express.json());
+// server.use(cors());
+// server.get('/', (req, res) => {
+//   res.send('API ONLINE');
+// });
 
-server.post('/api/register', (req, res) => {
-  let user = req.body;
+// server.post('/api/register', (req, res) => {
+//   let user = req.body;
 
-  const hash = bcrypt.hashSync(user.password, 4);
-  user.password = hash;
+//   const hash = bcrypt.hashSync(user.password, 4);
+//   user.password = hash;
 
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
+//   Users.add(user)
+//     .then(saved => {
+//       res.status(201).json(saved);
+//     })
+//     .catch(error => {
+//       res.status(500).json(error);
+//     });
+// });
 
-server.post('/api/login', (req, res) => {
-  let { username, password } = req.body;
+// server.post('/api/login', (req, res) => {
+//   let { username, password } = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
+//   Users.findBy({ username })
+//     .first()
+//     .then(user => {
+//       if (user && bcrypt.compareSync(password, user.password)) {
+//         res.status(200).json({ message: `Welcome ${user.username}!` });
+//       } else {
+//         res.status(401).json({ message: 'Invalid Credentials' });
+//       }
+//     })
+//     .catch(error => {
+//       res.status(500).json(error);
+//     });
+// });
 
-server.get('/api/login', restricted, (req, res) => {
-  Users.find()
-    .then(users => {
-      res.json(users);
-    })
-    .catch(err => res.send(err));
-});
-function only(username) {
-  return function(req, res, next) {
-    if (req.headers.username === username) {
-      next();
-    } else {
-      res.status(403).json({ message: `you are not ${username}` });
-    }
-  };
-}
-function authorized(req, res, next) {
-  const { username, password } = req.headers;
-  if (username && password) {
-    Users.findBy({ username });
-  } else {
-    return status(400).json({ message: 'you are not registered.' });
-  }
-}
-function restricted(req, res, next) {
-  const { username, password } = req.headers;
+// server.get('/api/login', restricted, (req, res) => {
+//   Users.find()
+//     .then(users => {
+//       res.json(users);
+//     })
+//     .catch(err => res.send(err));
+// });
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'You shall not pass!!' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  } else {
-    res.status(401).json({ message: 'Please provide credentials' });
-  }
-}
+// function restricted(req, res, next) {
+//   const { username, password } = req.headers;
+
+//   if (username && password) {
+//     Users.findBy({ username })
+//       .first()
+//       .then(user => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//           next();
+//         } else {
+//           res.status(401).json({ message: 'You shall not pass!!' });
+//         }
+//       })
+//       .catch(error => {
+//         res.status(500).json(error);
+//       });
+//   } else {
+//     res.status(401).json({ message: 'Please provide credentials' });
+//   }
+// }
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
